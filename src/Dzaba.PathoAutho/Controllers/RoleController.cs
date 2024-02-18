b7c1a66ef0b7e535599b0657ee282be03961014f
dzaba1 <blazej.zabialowicz@gmail.com>
2024-02-18 15:31:10 +0100

    Handling super admin

---
+++ src/Dzaba.PathoAutho/Controllers/RoleController.cs
@@ -0,0 +1,55 @@
+﻿using Dzaba.PathoAutho.ActionFilters;
+using Dzaba.PathoAutho.Lib;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Dzaba.PathoAutho.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[HandleErrors]
+public class RoleController : ControllerBase
+{
+    private readonly IRoleService roleService;
+    private readonly IUserService userService;
+
+    public RoleController(IRoleService roleService,
+        IUserService userService)
+    {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    [HttpPost("identity/{role}/user/{userName}")]
+    [Authorize(Roles = RoleNames.SuperAdmin)]
+    public async Task AssignUserToIdentiyRoleAsync(string userName, string role)
+    {
+        var user = await userService.FindUserByNameAsync(userName)
+            .ConfigureAwait(false);
+
+        if (user == null)
+        {
+            throw new HttpResponseException(HttpStatusCode.BadRequest, $"User {userName} not found.");
+        }
+
+        await roleService.AssignUserToIdentiyRoleAsync(user, role)
+            .ConfigureAwait(false);
+    }
+
+    [HttpDelete("identity/{role}/user/{userName}")]
+    [Authorize(Roles = RoleNames.SuperAdmin)]
+    public async Task RemoveUserFromIdentiyRoleAsync(string userName, string role)
+    {
+        var user = await userService.FindUserByNameAsync(userName)
+            .ConfigureAwait(false);
+
+        if (user == null)
+        {
+            throw new HttpResponseException(HttpStatusCode.BadRequest, $"User {userName} not found.");
+        }
+
+        await roleService.RemoveUserFromIdentiyRoleAsync(user, role)
+            .ConfigureAwait(false);
+    }
+}
