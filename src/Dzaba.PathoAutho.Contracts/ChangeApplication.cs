b7fd57c42e7347b20fc34c4bd9180c8013b3bac5
dzaba1 <blazej.zabialowicz@gmail.com>
2024-02-18 19:58:27 +0100

    Application controller

---
+++ src/Dzaba.PathoAutho.Contracts/ChangeApplication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dzaba.PathoAutho.Contracts;
+
+public sealed class ChangeApplication
+{
+    public Guid Id { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    public string NewName { get; set; }
+}
---
+++ src/Dzaba.PathoAutho/Controllers/ApplicationController.cs
@@ -0,0 +1,46 @@
+﻿using Dzaba.PathoAutho.ActionFilters;
+using Dzaba.PathoAutho.Contracts;
+using Dzaba.PathoAutho.Lib;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dzaba.PathoAutho.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[HandleErrors]
+public class ApplicationController : ControllerBase
+{
+    private readonly IApplicationService appService;
+
+    public ApplicationController(IApplicationService appService)
+    {
+        this.appService = appService;
+    }
+
+    [HttpPost("{appName}")]
+    [Authorize(Roles = RoleNames.SuperAdmin)]
+    public async Task<Guid> NewApplicationAsync(string appName)
+    {
+        return await appService.NewApplicationAsync(appName)
+            .ConfigureAwait(false);
+    }
+
+    [HttpDelete("{appId}")]
+    [Authorize(Roles = RoleNames.SuperAdmin)]
+    public async Task RemoveApplicationAsync(Guid appId)
+    {
+        await appService.RemoveApplicationAsync(appId)
+            .ConfigureAwait(false);
+    }
+
+    [HttpPut]
+    [ValidateModel]
+    [Authorize(Roles = RoleNames.SuperAdmin)]
+    public async Task ChangeApplicationAsync([FromBody, Required] ChangeApplication changeApplication)
+    {
+        await appService.ChangeNameAsync(changeApplication.Id, changeApplication.NewName)
+            .ConfigureAwait(false);
+    }
+}
